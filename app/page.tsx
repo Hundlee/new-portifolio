3a35f17fcd0af6fb3924f8a68896956491d8d6a8
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-07-08 17:05:38 -0300

    feat: add header component on home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@
 
 import Main from "./_components/Main";
 import NavBar from "./_components/NavBar";
-import Skills from "./_components/Skills";
+import AboutMe from "./_components/About-me";
+import Header from "./_components/Header";
 
 export default function Home() {
     return (
         <div className="flex flex-col relative">
+            <Header />
             <NavBar />
             <Main />
-            <Skills />
+            <AboutMe />
         </div>
     );
 }
