a1d16dbef749cbb248a5679425164b6f32db1934
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-07-08 17:06:18 -0300

    style: switch background

--- app/_components/NavBar/index.tsx
+++ app/_components/NavBar/index.tsx
@@ -3,7 +3,7 @@ import Sections from "./components/sections";
 
 const NavBar = () => {
     return (
-        <div className="fixed z-30 h-screen w-20 right-0 top-0 bg-background flex flex-col items-center ">
+        <div className="fixed z-30 h-screen w-20 right-0 top-0 bg-secondary flex flex-col items-center ">
             <Logo />
 
             <div className="mt-60">
