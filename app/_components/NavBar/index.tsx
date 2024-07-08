f714a2ad215764d5718264636a1abe23615ffb2e
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-07-08 14:50:05 -0300

    feat: add NavBar component

---
+++ app/_components/NavBar/index.tsx
@@ -0,0 +1,16 @@
+import Logo from "./components/logo";
+import Sections from "./components/sections";
+
+const NavBar = () => {
+    return (
+        <div className="fixed z-30 h-screen w-20 right-0 top-0 bg-background flex flex-col items-center ">
+            <Logo />
+
+            <div className="mt-60">
+                <Sections />
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
