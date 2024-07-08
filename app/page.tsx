cb3ff5a7152dd0d9594608617e6ca9ba003501af
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-07-08 14:49:07 -0300

    feat: add navbar on page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import Main from "./_components/Main";
+import NavBar from "./_components/NavBar";
 import Skills from "./_components/Skills";
 
 export default function Home() {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col relative">
+            <NavBar />
             <Main />
             <Skills />
         </div>
