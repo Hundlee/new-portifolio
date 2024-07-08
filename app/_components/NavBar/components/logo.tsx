4efe48e1ae0a9e45d3066f09e20e232b9036e663
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-07-08 17:50:18 -0300

    styles: add margin  top

--- app/_components/NavBar/components/logo.tsx
+++ app/_components/NavBar/components/logo.tsx
@@ -1,7 +1,7 @@
 const Logo = () => {
     return (
         <a href="#">
-            <button className="bg-zinc-900 p-4 mt-2 rounded-md">
+            <button className="bg-zinc-900 p-4 mt-4 rounded-md">
                 <h3 className="text-xl font-bold">
                     G<span className="text-primary">M</span>
                 </h3>
