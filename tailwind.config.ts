dcf79dd897ec74e7053cf59dc6be5efe4ffafd35
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-07-08 14:48:58 -0300

    chore: new animation name

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,15 +68,15 @@ const config = {
                     from: { height: "var(--radix-accordion-content-height)" },
                     to: { height: "0" },
                 },
-                slidein: {
-                    from: { opacity: "0", transform: "translateY(-50px)" },
+                slideintop: {
+                    from: { opacity: "0", transform: "translateY(-200px)" },
                     to: { opacity: "1", transform: "translateY(0)" },
                 },
             },
             animation: {
                 "accordion-down": "accordion-down 0.2s ease-out",
                 "accordion-up": "accordion-up 0.2s ease-out",
-                slidein: "slidein 1s ease 300ms",
+                slideintop: "slideintop 1s ease 300ms",
             },
         },
     },
