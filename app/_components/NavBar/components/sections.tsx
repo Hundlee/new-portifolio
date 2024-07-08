efa736cdb00682af1d57a62f6ae98aa5e939367a
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-07-08 19:16:53 -0300

    styles: add animation

--- app/_components/NavBar/components/sections.tsx
+++ app/_components/NavBar/components/sections.tsx
@@ -4,8 +4,12 @@ const Sections = () => {
     return (
         <div className="flex h-full transform -rotate-90 origin-center">
             {sections.map((item, index) => (
-                <a href={`#${item}`} key={index} className="h-full">
-                    <button className=" h-20 w-36 text-white hover:bg-background transition hover:border-t hover:border-primary capitalize">
+                <a
+                    href={`#${item}`}
+                    key={index}
+                    className="h-full animate-slidenavbarright3"
+                >
+                    <button className=" h-20 w-36 text-white hover:bg-background transition hover:border-t hover:border-primary capitalize ">
                         {item}
                     </button>
                 </a>
--- app/_components/NavBar/index.tsx
+++ app/_components/NavBar/index.tsx
@@ -3,7 +3,7 @@ import Sections from "./components/sections";
 
 const NavBar = () => {
     return (
-        <div className="fixed z-30 h-screen w-20 right-0 top-0 bg-secondary flex flex-col items-center ">
+        <div className="animate-slideinright1s fixed z-30 h-screen w-20 right-0 top-0 bg-secondary flex flex-col items-center ">
             <Logo />
 
             <div className="mt-60">
