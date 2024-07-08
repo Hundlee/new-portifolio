213657c1a3a9b5d9dceebadf70eadc5992ad4ff0
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-07-08 14:49:35 -0300

    feat: add dynamic-text component

---
+++ app/_components/Main/components/dynamic-Text.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+const DynamicText = () => {
+    const phrases = ["Programador Full Stack", "Desenvolvedor Full Stack"];
+    const [displayedText, setDisplayedText] = useState("");
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [loopNum, setLoopNum] = useState(0);
+    const [typingSpeed, setTypingSpeed] = useState(150);
+    const typingTimeoutRef = useRef<number | null>(null);
+
+    const handleType = () => {
+        const currentIndex = loopNum % phrases.length;
+        const fullText = phrases[currentIndex];
+
+        setDisplayedText(
+            isDeleting
+                ? fullText.substring(0, displayedText.length - 1)
+                : fullText.substring(0, displayedText.length + 1)
+        );
+
+        setTypingSpeed(isDeleting ? 75 : 150);
+
+        if (!isDeleting && displayedText === fullText) {
+            setTypingSpeed(1000);
+            setIsDeleting(true);
+        } else if (isDeleting && displayedText === "") {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setTypingSpeed(500);
+        }
+    };
+
+    useEffect(() => {
+        typingTimeoutRef.current = window.setTimeout(() => {
+            handleType();
+        }, typingSpeed);
+
+        return () => {
+            if (typingTimeoutRef.current) {
+                clearTimeout(typingTimeoutRef.current);
+            }
+        };
+    }, [displayedText, isDeleting, typingSpeed]);
+
+    return (
+        <h3 className="font-thin text-4xl pointer-events-none select-none">
+            Sou um{" "}
+            <span className="text-primary font-bold">{displayedText}</span>
+            <span id="cursor">|</span>
+        </h3>
+    );
+};
+
+export default DynamicText;
--- app/_components/Main/index.tsx
+++ app/_components/Main/index.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import { Canvas } from "@react-three/fiber";
 import React from "react";
 import { PointCircle } from "./components/point-circle";
 import MainDescription from "./components/main-description";
 import { Button } from "../ui/button";
 import { OrbitControls } from "@react-three/drei";
+import DynamicText from "./components/dynamic-Text";
 
 const Main = () => {
     return (
@@ -12,7 +15,7 @@ const Main = () => {
                 camera={{
                     position: [10, -7.5, -5],
                 }}
-                className="bg-background"
+                className="bg-zinc-900"
             >
                 <directionalLight />
                 <pointLight position={[-30, 0, -30]} power={10.0} />
@@ -20,17 +23,14 @@ const Main = () => {
             </Canvas>
 
             <div className="absolute top-[50%] left-[40%] -translate-x-[50%] -translate-y-[50%]">
-                <div className="animate-slidein gap-3 flex flex-col">
+                <div className="animate-slideintop gap-3 flex flex-col">
                     <h1 className="text-white font-bold text-6xl pointer-events-none select-none">
                         Olá, Meu nome é Gabriel Mesquita
                         <span className="text-primary text-7xl">.</span>
                     </h1>
-                    <h3 className="font-thin text-4xl pointer-events-none select-none">
-                        Sou um{" "}
-                        <span className="font-bold text-primary">
-                            Desenvolvedor Full Stack
-                        </span>
-                    </h3>
+
+                    <DynamicText />
+
                     <MainDescription />
 
                     <a href="#contact">
