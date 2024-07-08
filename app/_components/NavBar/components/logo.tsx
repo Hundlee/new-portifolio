3497ab43d233884a8d9b1444789691c99ffe175b
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-07-08 14:50:15 -0300

    feat: add logo component

---
+++ app/_components/NavBar/components/logo.tsx
@@ -0,0 +1,13 @@
+const Logo = () => {
+    return (
+        <a href="#">
+            <button className="bg-zinc-900 p-4 mt-2 rounded-md">
+                <h3 className="text-xl font-bold">
+                    G<span className="text-primary">M</span>
+                </h3>
+            </button>
+        </a>
+    );
+};
+
+export default Logo;
