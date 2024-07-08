ee39630a8951ca4e581400014fa3ea2cad7d6e58
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-07-08 17:06:40 -0300

    style: switch background

--- app/_components/NavBar/components/sections.tsx
+++ app/_components/NavBar/components/sections.tsx
@@ -5,7 +5,7 @@ const Sections = () => {
         <div className="flex h-full transform -rotate-90 origin-center">
             {sections.map((item, index) => (
                 <a href={`#${item}`} key={index} className="h-full">
-                    <button className=" h-20 w-36 text-white hover:bg-zinc-900 transition hover:border-t hover:border-primary capitalize">
+                    <button className=" h-20 w-36 text-white hover:bg-background transition hover:border-t hover:border-primary capitalize">
                         {item}
                     </button>
                 </a>
