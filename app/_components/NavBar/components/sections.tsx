c5e1d738b2673caffafaed7b3d216d56ae8e3749
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-07-13 16:34:10 -0300

    feat: add function to check which session I am in and activate the button in the navigation bar

--- app/_components/NavBar/components/sections.tsx
+++ app/_components/NavBar/components/sections.tsx
@@ -1,5 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 const Sections = () => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     const sections = ["contato", "projetos", "sobre-mim"];
+    const [currentSection, setCurrentSection] = useState("");
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY + 200;
+
+            sections.forEach((sectionId) => {
+                const section = document.getElementById(sectionId);
+                if (section) {
+                    const sectionTop = section.offsetTop;
+                    const sectionHeight = section.offsetHeight;
+
+                    if (
+                        scrollPosition >= sectionTop &&
+                        scrollPosition < sectionTop + sectionHeight
+                    ) {
+                        setCurrentSection(sectionId);
+                    }
+                }
+            });
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [sections]);
 
     return (
         <div className="flex h-full transform -rotate-90 origin-center">
@@ -7,9 +39,13 @@ const Sections = () => {
                 <a
                     href={`#${item}`}
                     key={index}
-                    className="h-full animate-slidenavbarright3"
+                    className={`h-full animate-slidenavbarright3 ${
+                        currentSection === item
+                            ? "bg-background text-primary border-t border-primary"
+                            : ""
+                    }`}
                 >
-                    <button className=" h-20 w-36 text-white hover:bg-background transition hover:border-t hover:border-primary capitalize ">
+                    <button className="h-20 w-36 text-white hover:bg-background transition hover:border-t hover:border-primary capitalize">
                         {item}
                     </button>
                 </a>
@@ -17,5 +53,4 @@ const Sections = () => {
         </div>
     );
 };
-
 export default Sections;
