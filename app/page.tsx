3be5ebcc1d5951f5826e9cfd3076630403926332
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-07-14 15:55:48 -0300

    chore: switch folder

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import NavBar from "./_components/NavBar";
 import AboutMe from "./_components/About-me";
 import Header from "./_components/Header";
 import Projects from "./_components/Projects";
-import Contato from "./_components/Contato";
+import Contato from "./_components/Contact";
 
 export default function Home() {
     return (
