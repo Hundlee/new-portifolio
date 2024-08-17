bc92342bae847d53e36d3d42d422ba2cfad21215
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-08-17 20:19:43 -0300

    style: add hover effect

--- app/_components/About-me/components/skill-icon.tsx
+++ app/_components/About-me/components/skill-icon.tsx
@@ -1,6 +1,6 @@
 const SkillIcon = ({ name }: { name: string }) => {
     return (
-        <div className="p-2 px-3 rounded-2xl bg-zinc-800 hover:bg-primary cursor-pointer">
+        <div className="p-2 px-3 rounded-2xl bg-zinc-800 hover:bg-primary delay-100 cursor-pointer ease-in">
             <h3 className="text-white">{name}</h3>
         </div>
     );
